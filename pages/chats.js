404087094301b0bbf11b5bc40d136fc813b6e15d
sagalosman <sagalosman0@gmail.com>
2023-07-31 19:15:28 +0100

    Add your commit

---
+++ pages/chats.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, useContext } from "react";
+
+import { Context } from "../context";
+
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
+export default function Home() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setShowChat(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (username === "" || secret === "") {
+      router.push("/");
+    }
+  }, [username, secret]);
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 212px)"
+          projectID="288e48f6-f716-4fee-a483-28c96b65681c"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+    </div>
+  );
+}
